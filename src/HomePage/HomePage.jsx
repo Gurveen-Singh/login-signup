a7a6877157ee6b55196def6f01c44c6aabd87b09
Gurveen Singh <55324834+Gurveen-Singh@users.noreply.github.com>
2021-01-18 15:19:40 +0530

    Add files via upload

---
+++ src/HomePage/HomePage.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { userActions } from '../_actions';
+
+function HomePage() {
+    const users = useSelector(state => state.users);
+    const user = useSelector(state => state.authentication.user);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(userActions.getAll());
+    }, []);
+
+    function handleDeleteUser(id) {
+        dispatch(userActions.delete(id));
+    }
+
+    return (
+        <div className="col-lg-8 offset-lg-2">
+            <h1>Hi {user.firstName}!</h1>
+            <p>You're logged In!!</p>
+            
+            <p>
+                <Link to="/login">Logout</Link>
+            </p>
+        </div>
+    );
+}
+
+export { HomePage };
